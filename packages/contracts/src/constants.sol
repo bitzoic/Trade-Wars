885ee30450aa06ba5fea21732c429b956d8acbd2
bitzoic <cameron.carstens@fuel.sh>
2023-05-18 14:15:52 -0700

    Add basic uprade system

--- packages/contracts/src/constants.sol
+++ packages/contracts/src/constants.sol
@@ -4,6 +4,10 @@ pragma solidity >=0.8.0;
 uint256 constant CHUNK_SIZE = 10;
 
 uint256 constant REGEN_COST = 50;
+uint256 constant FIRE_POWER_COST = 1000;
+uint256 constant FIRE_RATE_COST = 700;
+uint256 constant CARGO_COST = 400;
+uint256 constant HEALTH_COST = 300;
 
 // Materials
 uint256 constant IRON_WEIGHT = 100;
--- packages/contracts/src/systems/port/UpgradeSystem.sol
+++ packages/contracts/src/systems/port/UpgradeSystem.sol
@@ -2,10 +2,69 @@
 pragma solidity >=0.8.0;
 
 import { System } from "@latticexyz/world/src/System.sol";
+import { Position, Coins, Health, FirePower, CargoSpace } from "../../codegen/Tables.sol";
+import { CHUNK_SIZE, HEALTH_COST, CARGO_COST, FIRE_POWER_COST, FIRE_RATE_COST } from "../../constants.sol";
 
 contract UpgradeSystem is System {
 
-    function upgrade() public {
+    function upgrade_fire_power(uint256 amount) public {
+        uint256 currentPositionX = Position.getPos_x(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentPositionY = Position.getPos_y(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentFirePower = FirePower.getPower(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentCoins = Coins.get(keccak256(abi.encodePacked(msg.sender)));
 
+        // TODO: Require we are in the same chunk as a port
+
+        require(currentCoins >= FIRE_POWER_COST * amount, "Not enough coins");
+
+        // Remove the cost of fire power
+        Coins.set(keccak256(abi.encodePacked(msg.sender)), currentCoins - (FIRE_POWER_COST * amount));
+        FirePower.setPower(keccak256(abi.encodePacked(msg.sender)), currentFirePower + amount);
+    }
+
+    function upgrade_fire_rate(uint256 amount) public {
+        uint256 currentPositionX = Position.getPos_x(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentPositionY = Position.getPos_y(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentFireRate = FirePower.getRate(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentCoins = Coins.get(keccak256(abi.encodePacked(msg.sender)));
+
+        // TODO: Require we are in the same chunk as a port
+
+        require(currentCoins >= FIRE_RATE_COST * amount, "Not enough coins");
+
+        // Remove the cost of fire rate
+        Coins.set(keccak256(abi.encodePacked(msg.sender)), currentCoins - (FIRE_RATE_COST * amount));
+        FirePower.setPower(keccak256(abi.encodePacked(msg.sender)), currentFireRate + amount);
+    }
+
+    function upgrade_max_health(uint256 amount) public {
+        uint256 currentPositionX = Position.getPos_x(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentPositionY = Position.getPos_y(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentMaxHealth = Health.getMax_health(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentCoins = Coins.get(keccak256(abi.encodePacked(msg.sender)));
+
+        // TODO: Require we are in the same chunk as a port
+
+        require(currentCoins >= HEALTH_COST * amount, "Not enough coins");
+
+        // Remove the cost of health and set health to max
+        Coins.set(keccak256(abi.encodePacked(msg.sender)), currentCoins - (HEALTH_COST * amount));
+        Health.setMax_health(keccak256(abi.encodePacked(msg.sender)), currentMaxHealth + amount);
+        Health.setHealth(keccak256(abi.encodePacked(msg.sender)), currentMaxHealth + amount);
+    } 
+
+    function upgrade_cargo(uint256 amount) public {
+        uint256 currentPositionX = Position.getPos_x(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentPositionY = Position.getPos_y(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentMaxCargo = CargoSpace.getMax_cargo(keccak256(abi.encodePacked(msg.sender)));
+        uint256 currentCoins = Coins.get(keccak256(abi.encodePacked(msg.sender)));
+
+        // TODO: Require we are in the same chunk as a port
+
+        require(currentCoins >= CARGO_COST * amount, "Not enough coins");
+
+        // Remove the cost of fire rate
+        Coins.set(keccak256(abi.encodePacked(msg.sender)), currentCoins - (CARGO_COST * amount));
+        CargoSpace.setMax_cargo(keccak256(abi.encodePacked(msg.sender)), currentMaxCargo + amount);
     }
 }
