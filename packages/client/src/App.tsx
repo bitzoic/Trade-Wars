63a1a1af1ca10c4f4a4793a32424a081a0a32ca0
w <william.y.xuan@gmail.com>
2023-05-25 22:33:29 -0700

    mmmhmmm

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -17,6 +17,13 @@ import {
   Footer,
 } from "nes-ui-react";
 
+import Sand from "./assets/sand.png";
+import ShallowWater from "./assets/AnimatedWaterShallow.gif";
+import Land from "./assets/land.png";
+import DeepWater from "./assets/AnimatedWaterDark.gif";
+import Ship from "./assets/Boats.png";
+import Port from "./assets/port.png";
+
 import "./App.css";
 
 import { SwapModal } from "./components/SwapModal";
@@ -31,15 +38,80 @@ export const App = () => {
 
   const [x1, setX1] = useState("0");
   const [y1, setY1] = useState("0");
-  const [x2, setX2] = useState("10");
-  const [y2, setY2] = useState("4");
+  const [x2, setX2] = useState("30");
+  const [y2, setY2] = useState("10000");
   const [map, setMap] = useState<number[][]>([]);
   const [demoDialogOpen, setDemoDialogOpen] = useState(false);
   const [showIntroductionModal, setShowIntroductionModal] = useState(false);
+  const [shipVisible, setShipVisible] = useState(false);
+
+  const terrainImageStyle = {
+    width: "20px",
+    height: "20px",
+  };
+  const shipStyle = {
+    width: "30px",
+    height: "30px",
+  };
+
+  const handleSpawnClick = () => {
+    setShipVisible(true);
+  };
 
   useEffect(() => {
     setShowIntroductionModal(true);
+    handleGetMap();
   }, []);
+
+  useEffect(() => {
+    handleGetMap();
+  }, [x1, y1, x2, y2]);
+
+  const findImage = (value: number) => {
+    const terrainType = Math.floor(value / 10);
+    let terrainImage;
+
+    switch (terrainType) {
+      case 1:
+        terrainImage = Sand;
+        break;
+      case 2:
+        terrainImage = Land;
+        break;
+      case 3:
+        terrainImage = Land;
+        break;
+      case 4:
+        terrainImage = ShallowWater;
+        break;
+      case 5:
+        terrainImage = DeepWater;
+        break;
+      default:
+        terrainImage = ShallowWater;
+    }
+
+    if (value % 10 === 1) {
+      return (
+        <div style={terrainImageStyle}>
+          <img
+            src={terrainImage}
+            alt={`Terrain ${terrainType}`}
+            style={terrainImageStyle}
+          />
+          <img src={Port} alt="Port" style={terrainImageStyle} />
+        </div>
+      );
+    }
+
+    return (
+      <img
+        src={terrainImage}
+        alt={`Terrain ${terrainType}`}
+        style={terrainImageStyle}
+      />
+    );
+  };
 
   const handleGetMap = async () => {
     const result = await getMap(Number(x1), Number(y1), Number(x2), Number(y2));
@@ -54,60 +126,28 @@ export const App = () => {
         <IntroductionModal />
       </Modal>
 
+      <div></div>
       <div>
-        {/* <label>
-          X1:
-          <input
-            type="text"
-            value={x1}
-            onChange={(e) => setX1(e.target.value)}
-          />
-        </label>
-        <label>
-          Y1:
-          <input
-            type="text"
-            value={y1}
-            onChange={(e) => setY1(e.target.value)}
-          />
-        </label>
-        <label>
-          X2:
-          <input
-            type="text"
-            value={x2}
-            onChange={(e) => setX2(e.target.value)}
-          />
-        </label>
-        <label>
-          Y2:
-          <input
-            type="text"
-            value={y2}
-            onChange={(e) => setY2(e.target.value)}
-          />
-        </label> */}
-        <Button type="button" onClick={handleGetMap}>
-          Show Map
+        <Container align="center" title="Game Map">
+          {console.log(map)}
+          {map.map((row, rowIndex) => (
+            <div key={rowIndex} className="map-row">
+              {row.map((value, colIndex) => (
+                <span key={`${rowIndex}-${colIndex}`}>{findImage(value)}</span>
+              ))}
+              <img
+                src={Ship}
+                alt="Ship"
+                className={shipVisible ? "visible" : "hidden"}
+                style={{ position: "absolute", top: "200px", left: "50px" }}
+              />
+            </div>
+          ))}
+        </Container>
+        <Button color="success" onClick={handleSpawnClick}>
+          Spawn
         </Button>
       </div>
-      <Container align="center" title="Game Map">
-        {map.map((row, rowIndex) => (
-          <div key={rowIndex} className="map-row">
-            {row.map((value, colIndex) => (
-              <span
-                key={`${rowIndex}-${colIndex}`}
-                style={{
-                  padding: "0.5rem",
-                  margin: "0.1rem",
-                }}
-              >
-                {value}
-              </span>
-            ))}
-          </div>
-        ))}
-      </Container>
       <div>
         <IconButton
           borderInverted
